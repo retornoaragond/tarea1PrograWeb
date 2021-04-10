277ec9b01f174acbbf72dc2b08622faf7658287e
retornoaragond <31984922+retornoaragond@users.noreply.github.com>
2021-04-09 18:42:22 -0600

    tarea termianda

--- routes.php
+++ routes.php
@@ -1,6 +1,11 @@
 <?php
     
-    Route::get('/',function() { echo 'Hello, World!'; });
+    Route::get('/',function() { return view('main_page'); });
+    Route::get('main_page',function() { return view('main_page'); });
+    Route::get('book_one',function() { return view('book_one'); });
+    Route::get('book_two',function() { return view('book_two'); });
+    Route::get('book_three',function() { return view('book_three'); });
+    Route::get('book_four',function() { return view('book_four'); });
     
     Route::dispatch();
 ?>
